241a15422f62b1cc4feede2e583d7b15b1180127
Alexander Q <qalexx@gmail.com>
2025-05-01 17:36:19 +0300

    configuration file

---
+++ lua/plugins_config.lua
@@ -0,0 +1,18 @@
+return {
+    r = {
+        executable = "R",
+        enabled = false,
+    },
+    python = {
+        executable = "python",
+        enabled = true,
+    },
+    rust = {
+        executable = "rustc",
+        enabled = true,
+    },
+    erlang = {
+        executable = "erl",
+        enabled = false,
+    }
+}
